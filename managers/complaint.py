4311992a37d86a412e21fdd8bc592abc28c28aca
sebah12 <sebah_12@hotmail.com>
2023-07-31 21:53:30 -0300

    Basic implementation of SES sending email on complaint approved.

--- managers/complaint.py
+++ managers/complaint.py
@@ -3,10 +3,13 @@ import os
 from models import complaint, RoleType, State
 from db import database
 from services.s3 import S3Service
+from services.ses import SESService
 from constants import TEMP_FILE_FOLDER
 from utils.helpers import decode_photo
 
 s3 = S3Service()
+ses = SESService()
+
 
 class ComplaintManager:
     @staticmethod
@@ -28,24 +31,38 @@ class ComplaintManager:
         decode_photo(path, encoded_photo)
         complaint_data["photo_url"] = s3.upload(path, name, extension)
         os.remove(path)
-        id_ = await database.execute(complaint.insert().values(complaint_data))
+        id_ = await database.execute(
+            complaint.insert().values(complaint_data)
+        )
         return await database.fetch_one(
-            complaint.select().where(complaint.c.id == id_))
+            complaint.select().where(complaint.c.id == id_)
+        )
 
     @staticmethod
     async def delete(complaint_id):
-        await database.execute(complaint.delete().
-                               where(complaint.c.id == complaint_id))
+        await database.execute(
+            complaint.delete().
+            where(complaint.c.id == complaint_id)
+        )
 
 
     @staticmethod
     async def approve(id_):
-        await database.execute(complaint.update().
-                               where(complaint.c.id == id_).
-                               values(status=State.approved))
+        await database.execute(
+            complaint.update().
+            where(complaint.c.id == id_).
+            values(status=State.approved)
+        )
+        ses.send_mail(
+            "Complaint approved!",
+            ["sebah_12@hotmail.com"],
+            "Congrats! Your claim is approved, check your bank account in 2 days"
+        )
 
     @staticmethod
     async def reject(id_):
-        await database.execute(complaint.update().
-                               where(complaint.c.id == id_).
-                               values(status=State.rejected))
+        await database.execute(
+            complaint.update().
+            where(complaint.c.id == id_).
+            values(status=State.rejected)
+        )
